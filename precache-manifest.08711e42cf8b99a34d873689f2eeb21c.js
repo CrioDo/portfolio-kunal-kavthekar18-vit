654494a48df499ce0c90f7b385d3cc458bb0c01e
CrioDo <rahul@criodo.com>
2020-10-29 20:44:21 +0530

    Updates

---
+++ precache-manifest.08711e42cf8b99a34d873689f2eeb21c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b632f912f3ee3eb648498f48c5165e88",
+    "url": "/portfolio-kunal-kavthekar18-vit/index.html"
+  },
+  {
+    "revision": "96bbaefca1d6db6e014b",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "7736fb24ec365d8cd2f6",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "96bbaefca1d6db6e014b",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/js/2.ad13abdb.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/js/2.ad13abdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7736fb24ec365d8cd2f6",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/js/main.49f8f50a.chunk.js"
+  },
+  {
+    "revision": "12f77bf496a8ab3af532",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/js/runtime-main.527bbf86.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-kunal-kavthekar18-vit/static/media/crio-logo.63be71d0.svg"
+  }
+]);
